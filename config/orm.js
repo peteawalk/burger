ce1411ec4c35bf6ceb204fa8ae5ac64a62d3c0cc
Peter Walker <25408515+peteawalk@users.noreply.github.com>
2019-09-12 23:29:23 -0500

    removed delete functionality from orm

--- config/orm.js
+++ config/orm.js
@@ -29,8 +29,6 @@ function objToSql(ob) {
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
-      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-      // e.g. {sleepy: true} => ["sleepy=true"]
       arr.push(key + "=" + value);
     }
   }
@@ -87,20 +85,7 @@ var orm = {
 
       cb(result);
     });
-  },
-  delete: function(table, condition, cb) {
-    var queryString = "DELETE FROM " + table;
-    queryString += " WHERE ";
-    queryString += condition;
-
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
-
-      cb(result);
-    });
-  }
+  } 
 };
 
 // Export the orm object for the model (cat.js).
